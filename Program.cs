865dc13213c043038272c4f47115b679e7826d53
ivan.iljin <ivan.iljin@aristocrat.com>
2022-02-28 22:53:59 +0100

    Implement startup in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using error.tests.grpc;
+using Grpc.Core;
+
 Console.WriteLine("Hello, World!");
+
+var service = new TestGrpcStatuses.TestGrpcStatusesServiceImpl();
+
+var hostname = "localhost";
+var port = 8888;
+
+var server = new Server()
+{
+    Services = { TestGrpcStatusService.BindService(service) },
+    Ports = {
+        new ServerPort(
+            hostname,
+            port,
+            ServerCredentials.Insecure
+        )
+    },
+};
+
+server.Start();
+Console.WriteLine($"Server is listening from {hostname} on port {port}");
+
+Console.WriteLine("Press enter to stop the server...");
+Console.Read();
+
+await server.KillAsync();
+return 0;
